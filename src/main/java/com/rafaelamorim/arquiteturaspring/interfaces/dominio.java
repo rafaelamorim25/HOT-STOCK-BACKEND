899ce5afcbf68797fbda5f373d4097f50e9f0881
Vinicius <vinizge@gmail.com>
2019-06-22 16:19:24 -0300

    adicionado interfaces e modelos

---
+++ src/main/java/com/rafaelamorim/arquiteturaspring/interfaces/dominio.java
@@ -0,0 +1,13 @@
+package com.rafaelamorim.arquiteturaspring.interfaces;
+
+public interface dominio {
+	
+	public void setId();
+	
+	public Integer getId();
+	
+	public void setNome();
+
+	public String getNome();
+	
+}
